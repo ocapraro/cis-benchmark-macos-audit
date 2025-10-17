cd1d4ea3d4ca9b663b49354dc917a037d223e60e
ocapraro <oscar@mcapraro.com>
2025-10-16 23:09:50 -0400

    fix 4.3

--- backend/scripts/4_3/detect.sh
+++ backend/scripts/4_3/detect.sh
@@ -1,3 +1,3 @@
-/usr/bin/sudo /bin/launchctl list | /usr/bin/grep -c com.apple.nfsd
+/usr/bin/sudo /bin/launchctl list | /usr/bin/grep -c com.apple.nfsd || true
 
-/usr/bin/sudo /bin/cat /etc/exports
+/usr/bin/sudo /bin/cat /etc/exports || true
