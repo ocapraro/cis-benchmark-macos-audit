fcc94daab4cb5c630a48abf8488165deb6bf1e08
ocapraro <oscar@mcapraro.com>
2025-10-16 23:14:15 -0400

    fix 5_11 detect

--- backend/scripts/5_11/detect.sh
+++ backend/scripts/5_11/detect.sh
@@ -1,1 +1,1 @@
-/usr/bin/sudo -u ocapraro /usr/bin/defaults read /Users/ocapraro/Library/Preferences/.GlobalPreferences.plist AppleShowAllExtensions
+/usr/bin/sudo -u ocapraro /usr/bin/defaults read /Users/ocapraro/Library/Preferences/.GlobalPreferences.plist AppleShowAllExtensions || true
