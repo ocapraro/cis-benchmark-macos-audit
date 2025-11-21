291084a67c3e3afe09e1929076fceea14952e67e
ocapraro <oscar@mcapraro.com>
2025-11-21 18:43:37 -0500

    fix 11212

--- backend/scripts/1_1_2_1_2/fix.sh
+++ backend/scripts/1_1_2_1_2/fix.sh
@@ -1,1 +1,16 @@
-sed -i 's|<device> /tmp    <fstype>     .*|<device> /tmp    <fstype>     defaults,rw,nosuid,nodev,noexec,relatime  0 0|' /etc/fstab && mount -o remount /tmp
+#!/bin/bash
+# Add nodev option to /tmp mount point
+
+# Check if /tmp entry exists in /etc/fstab
+if grep -q "^[^#]*[[:space:]]/tmp[[:space:]]" /etc/fstab; then
+    # /tmp is in fstab, add nodev if not present
+    sed -i.bak '/^[^#]*[[:space:]]\/tmp[[:space:]]/ {
+        /nodev/! s/\([[:space:]][[:alnum:],]*\)/\1,nodev/
+    }' /etc/fstab
+else
+    # /tmp not in fstab, using tmpfs
+    echo "tmpfs /tmp tmpfs defaults,rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
+fi
+
+# Remount /tmp with new options
+mount -o remount,nodev /tmp || systemctl daemon-reload && systemctl restart tmp.mount
